bfb9cef645822d8dc025845497f1326249abe7fd
Justin Murray <justin.murray@mmodal.com>
2015-06-23 10:35:14 -0400

    Add more browsers to test with Sauce

--- Gruntfile.js
+++ Gruntfile.js
@@ -168,6 +168,24 @@ module.exports = function (grunt) {
                             browserName: 'internet explorer',
                             platform: 'Windows 8.1',
                             version: '11'
+                        },
+                        sl_ie_10: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '10'
+                        },
+                        sl_ie_9: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '9'
+                        },
+                        sl_ie_8: {
+                            base: 'SauceLabs',
+                            browserName: 'internet explorer',
+                            platform: 'Windows 7',
+                            version: '8'
                         }
                     },
 
